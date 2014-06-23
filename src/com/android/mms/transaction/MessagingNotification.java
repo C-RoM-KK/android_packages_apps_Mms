4dbae4edda64bee88b12b42fc6d0be8bb497ccdd
Jorge Ruesga <jorge@ruesga.com>
2014-06-24 00:03:57 +0200

    Mms: update new messages notification count
    Mms should notify the system about how many new sms/mms has (including those ones under conversations)
    to the notification system. This allow the system to display the number of events for Mms app according its
    settings
    
    Change-Id: I15953283ae657b770869f611a886452199550d67
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- src/com/android/mms/transaction/MessagingNotification.java
+++ src/com/android/mms/transaction/MessagingNotification.java
@@ -1201,6 +1201,9 @@ public class MessagingNotification {
             notification = noti.build();
         }
 
+        // Update number of notifications
+        notification.number = messageCount;
+
         // Post the notification
         nm.notify(NOTIFICATION_ID, notification);
     }
